f0e247f46f8fd8fc15a676f3af275464a0a2831b
Kokosalah45 <kokp.amged@gmail.com>
2022-10-24 00:41:49 +0200

    added dynamic hostname

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -1,18 +1,20 @@
 import { Router } from "express";
 import wordRouter from "./wordRouter";
 import { validateCache } from "../middlewares";
+import os from "os";
 const apiRouter = Router();
 
 apiRouter.use("/word", validateCache, wordRouter);
 
 apiRouter.get("/", (req, res) => {
+  const hostName = os.hostname;
+
   res.json({
     words: {
-      href: "http://localhost:5000/api/words",
-      type: "GET",
+      href: `${hostName}/api/words`,
     },
     rank: {
-      href: "http://localhost:5000/api/rank",
+      href: `${hostName}/api/rank`,
       type: "POST",
     },
   });
