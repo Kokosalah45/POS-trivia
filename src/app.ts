320d3d57b2a22feaa77a0bd709193a7e414802e6
Kokosalah45 <kokp.amged@gmail.com>
2022-10-24 13:52:41 +0200

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express, { Request, Response } from "express";
-
-import apiRouter from "./routes/apiRouter";
+import cors from "cors";
 import path from "path";
+import apiRouter from "./routes/apiRouter";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", apiRouter);
 app.get("/", async (req: Request, res: Response) => {
-  // for now
-  res.redirect("/api");
+  res.redirect("/api"); // for now
 });
 
 app.listen(process.env.PORT || 5000, () => console.log("listenin..."));
